6233a0f9f70291ce1e783d56565c4e14de3892f0
Kiochan <sunxingchen@live.com>
2019-05-22 23:28:40 +0200

    Create index.js

---
+++ src/text/index.js
@@ -0,0 +1,36 @@
+const text = {
+  hate: {
+    default: '...hate it!',
+    zh: '讨厌它！',
+  },
+  fine: {
+    default: '...like it!',
+    zh: '喜欢它',
+  },
+  title: {
+    default: 'I hate 7',
+    zh: '我讨厌 7',
+  },
+  titleSub: {
+    default: 'a mental arithmetic mini-game',
+    zh: '心算小游戏',
+  },
+  intro: {
+    default: 'I see a incomming number, it\'s...',
+    zh: '森林里，摇摇晃晃的走进来了一个数字，它是…',
+  },
+  then: {
+    default: 'and I...',
+    zh: '然后我…'
+  },
+  outro: {
+    default: 'I don\'t like number 7 and its multiple.',
+    zh: '我讨厌数字 7 和 7 的倍数。',
+  },
+  result: {
+    default: 'right: %d / wrong: %d / rate: %d',
+    zh: '正确: %d / 错误: %d / 正确率 %f',
+  }
+}
+
+export default text
