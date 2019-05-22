3448fda6559f70b2e4e5b650b68cacfbfbaeecde
Kiochan <sunxingchen@live.com>
2019-05-22 23:28:38 +0200

    Create styles.js

---
+++ src/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFFFFF",
+    flex: 1
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  center: {
+    alignItems: 'center',
+    padding: 30
+  },
+  actionButton: {
+    flex: 1,
+    marginLeft: 30,
+    marginRight: 30
+  },
+  number: {
+    fontSize: 100
+  },
+  intro: {
+    fontSize: 30
+  },
+  flex1: {
+    flex: 1,
+    justifyContent:'center'
+  },
+  flex2: {
+    flex: 2
+  }
+})
+
+export default styles
